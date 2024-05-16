84770172129a053c68fb830b35fa7443e569b82d
Adejumok <roseadeyinka01@gmail.com>
2024-05-16 12:05:30 +0100

    application initial files

---
+++ src/main/java/com/loco/SparkCraft/SparkCraftApplication.java
@@ -0,0 +1,14 @@
+package com.loco.SparkCraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkCraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkCraftApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/loco/SparkCraft/SparkJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loco.SparkCraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
